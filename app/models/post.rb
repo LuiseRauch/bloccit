7f4ae43f72d414e9fab9452eebc2e78b651ca9e2
Luise Rauch <luise.rauch@gmail.com>
2015-10-22 16:52:12 +0200

    Assignment incomplete

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,11 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  default_scope { order('created_at DESC') }
+  # default_scope { order('created_at DESC') }
   scope :ordered_by_title, -> { order('title DESC') }
   scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
+
+  # scope :recent, -> { where('created_at > ?', 1.week.ago)}
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
