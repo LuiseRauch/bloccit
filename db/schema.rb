bd7beb9c5e3c38161275f36b02ed48d8ca5a5ef5
Luise Rauch <luise.rauch@gmail.com>
2015-10-22 12:52:46 +0200

    Checkpoint 40 completed, Authorization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021115903) do
+ActiveRecord::Schema.define(version: 20151022100616) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20151021115903) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
